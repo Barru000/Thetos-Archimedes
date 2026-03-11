a4904030463958f7389ecd5a5819ddbc75e510bb
RoryBarrus <barru000@byu.edu>
2026-03-11 12:48:21 -0600

    HTTPS

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 // app.js
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const secureHeaders = require('./secure-headers');
+
 const app = express();
 app.use(secureHeaders);
 app.use(express.static('public'));
-app.listen(3000);
+
+const certOptions = {
+	key: fs.readFileSync(path.join(__dirname, 'cert/server.key')),
+	cert: fs.readFileSync(path.join(__dirname, 'cert/server.crt'))
+};
+
+https.createServer(certOptions, app).listen(3443, () => {
+	console.log('HTTPS server running at https://localhost:3443');
+});
